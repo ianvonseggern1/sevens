cb8bfc61db980b0281251007572a0053a9b67d5b
ianvonseggern1 <ian@spell.run>
2019-06-14 15:32:06 -0400

    try verbose 1

--- tic_tac_toe_dqn.py
+++ tic_tac_toe_dqn.py
@@ -43,7 +43,7 @@ dqn.compile(Adam(lr=1e-3), metrics=['mae'])
 # Okay, now it's time to learn something! We visualize the training here for show, but this
 # slows down training quite a lot. You can always safely abort the training prematurely using
 # Ctrl + C.
-dqn.fit(env, nb_steps=50000, visualize=False, verbose=2)
+dqn.fit(env, nb_steps=50000, visualize=False, verbose=1)
 
 # After training is done, we save the final weights.
 dqn.save_weights('dqn_tic_tac_toe_weights.h5f', overwrite=True)
