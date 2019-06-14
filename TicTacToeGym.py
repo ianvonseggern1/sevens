94f8eca38a1a4d86f6248eb0af18136302f47d77
ianvonseggern1 <ian@spell.run>
2019-06-14 15:23:13 -0400

    add tic tac toe new files

---
+++ TicTacToeGym.py
@@ -0,0 +1,96 @@
+import gym
+import gym.spaces
+
+import numpy as np
+
+
+class TicTacToeGym(gym.Env):
+    def __init__(self):
+        self.metadata = {'render.modes': ['human']}
+
+        # Board is 3 x 3, map action space to 3 * r + c, which looks like
+        #
+        # 0 1 2
+        # 3 4 5
+        # 6 7 8
+        self.action_space = gym.spaces.Discrete(9)
+
+        # -1 is opponents piece, 1 is our piece, 0 is empty
+        self.observation_space = gym.spaces.Box(
+            low=-1, high=1, shape=(3, 3), dtype=int)
+
+    # Returns (observation, reward, done)
+    # Returns a reward of -1 for an illegal move
+    # Returns 100 for a victory -100 for a loss and 0 for a tie
+    def step(self, action):
+        info = {"items": []}
+
+        row = action // 3
+        col = action % 3
+        if self.board[row][col] != 0:
+            return (self.board, -1, False, info)
+
+        self.board[row][col] = 1
+        isGameOver, winner = self.isGameOver()
+        if isGameOver:
+            return (self.board, winner * 100, True, info)
+
+        self.performOpponentMove()
+        isGameOver, winner = self.isGameOver()
+        # (observation, score, isDone, info)
+        return (self.board, winner * 100, isGameOver, info)
+
+    def reset(self):
+        self.board = np.zeros((3, 3))
+        #print("Reset: ")
+        # print(self.board)
+        return self.board
+
+    def render(self, mode='human'):
+        def getPiece(val):
+            if val is 1:
+                return 'X'
+            if val is -1:
+                return 'O'
+            return ' '
+        if mode is 'human':
+            for r in range(3):
+                print(' '.join([getPiece(v) for v in self.board[r]]))
+        else:
+            super(TicTacToeGym, self).render(mode=mode)
+
+    # Returns tuple (gameOver bool, winner int) where winner is -1 for opponent, 1 for you, and 0 for a tie
+    def isGameOver(self):
+        b = self.board
+        # Check if there is a winner
+        lines = [
+            [(0, 0), (0, 1), (0, 2)],
+            [(1, 0), (1, 1), (1, 2)],
+            [(2, 0), (2, 1), (2, 2)],
+            [(0, 0), (1, 0), (2, 0)],
+            [(0, 1), (1, 1), (2, 1)],
+            [(0, 2), (1, 2), (2, 2)],
+            [(0, 0), (1, 1), (2, 2)],
+            [(2, 0), (1, 1), (0, 2)],
+        ]
+        for l in lines:
+            if b[l[0]] == 1 and b[l[1]] == 1 and b[l[2]] == 1:
+                return (True, 1)
+            if b[l[0]] == -1 and b[l[1]] == -1 and b[l[2]] == -1:
+                return (True, -1)
+
+        # Check if there are no open square, in which case its a tie
+        for r in range(3):
+            for c in range(3):
+                if b[r][c] == 0:
+                    return (False, 0)
+        return (True, 0)
+
+    # For now this just moves into a random spot
+    def performOpponentMove(self):
+        while True:
+            row = np.random.randint(0, 3)
+            column = np.random.randint(0, 3)
+            if self.board[row][column] == 0:
+                self.board[row][column] = -1
+                return
