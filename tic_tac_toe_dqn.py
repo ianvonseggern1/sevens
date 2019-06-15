645321e793595f158887f5411422c09115f52e23
ianvonseggern1 <ian@spell.run>
2019-06-15 11:38:15 -0400

    really long run

--- tic_tac_toe_dqn.py
+++ tic_tac_toe_dqn.py
@@ -65,7 +65,7 @@ metrics_callback = LambdaCallback(
 # Okay, now it's time to learn something! We visualize the training here for show, but this
 # slows down training quite a lot. You can always safely abort the training prematurely using
 # Ctrl + C.
-dqn.fit(env, nb_steps=500000, visualize=False,
+dqn.fit(env, nb_steps=5000000, visualize=False,
         verbose=1, callbacks=[metrics_callback])
 
 # After training is done, we save the final weights.
